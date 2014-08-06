7f666785ad467d745d457af4772ce7c4bbd9aa7f
mihasya <m@mihasya.com>
2014-08-06 11:53:17 -0700

    update to new handler version

--- recipes/handler.rb
+++ recipes/handler.rb
@@ -12,8 +12,8 @@ chef_handler "Chef::Handler::Opsmatic" do
   source "chef/handler/opsmatic"
   arguments [ 
     :integration_token => node[:opsmatic][:integration_token],
-    :collector_url     => node[:opsmatic][:collector_url],
-    :ssl_peer_verify   => node[:opsmatic][:ssl_peer_verify]
+    :collector_url     => node[:opsmatic][:handler_endpoint],
+    :ssl_peer_verify   => node[:opsmatic][:handler_ssl_peer_verify]
   ]
   action :nothing
 end.run_action(:enable)
