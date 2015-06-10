aebdea2b14b3ce4a379d98597db5d44947ad3068
mihasya <m@mihasya.com>
2015-06-10 15:21:29 -0700

    turns out we already have a parameter for this

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'support@opsmatic.com'
 license 'All rights reserved'
 description 'Installs/Configures Opsmatic services and integrations'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.24'
+version          '0.1.25'
 
 depends 'chef_handler', '<= 1.1.8'
 depends 'apt'
